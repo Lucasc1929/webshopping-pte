0236b3065741dd205feb232c659ca9471e7534ff
Lucas Cecchin <lucasc.1929@gmail.com>
2022-11-27 12:58:47 -0300

    Changed home path

--- src/App.js
+++ src/App.js
@@ -15,22 +15,23 @@ import ModifyProducts from "./Routes/ModifyProducts.jsx";
 
 function App() {
   return (
-      <Router>
-        <Header />
-        <LoginStatus />
-        <Routes>
-          <Route path='/' element={<Products />} /> {/* products */}
-          <Route path='/products/:id' element={<SpecificProducts />} />
-          {/* specific product */}
-          <Route path='/cart' element={<Cart />} /> {/* cart */}
-          <Route path='/Login' element={<Login />} /> {/* login */}
-          <Route path='/Signup' element={<Signup />} /> {/* login */}
-          <Route path='/edit' element={<AddProduct />} />
-          {<Route path='/edit/:id' element={<ModifyProducts />} />}
-          {/* update products ("add, update, delete") */}
-          <Route path='*' element={<NotFound />} /> {/*404 */}
-        </Routes>
-      </Router>
+    <Router>
+      <Header />
+      <LoginStatus />
+      <Routes>
+        <Route path='/webshopping-pte' element={<Products />} />{" "}
+        {/* products */}
+        <Route path='/products/:id' element={<SpecificProducts />} />
+        {/* specific product */}
+        <Route path='/cart' element={<Cart />} /> {/* cart */}
+        <Route path='/Login' element={<Login />} /> {/* login */}
+        <Route path='/Signup' element={<Signup />} /> {/* login */}
+        <Route path='/edit' element={<AddProduct />} />
+        {<Route path='/edit/:id' element={<ModifyProducts />} />}
+        {/* update products ("add, update, delete") */}
+        <Route path='*' element={<NotFound />} /> {/*404 */}
+      </Routes>
+    </Router>
   );
 }
 
